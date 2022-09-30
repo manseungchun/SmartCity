83c8c952676c79d1301ea5c08542f3c73fc27b3c
riverpurple <rkdqhfk1245@gmail.com>
2022-09-30 12:02:12 +0900

    ddd

--- app/src/main/java/com/example/app0907/LodingActivity.java
+++ app/src/main/java/com/example/app0907/LodingActivity.java
@@ -2,7 +2,9 @@ package com.example.app0907;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 
 public class LodingActivity extends AppCompatActivity {
 
@@ -10,5 +12,19 @@ public class LodingActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_loding);
+        // 로딩화면 시작
+        Loadingstart();
+    }
+
+    private void Loadingstart() {
+        Handler handler=new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },2000);
     }
 }
