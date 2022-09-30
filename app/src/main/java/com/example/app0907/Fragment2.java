3bf9f2e9d83a2a845e2b271f95dc3b3068a4d5a8
riverpurple <rkdqhfk1245@gmail.com>
2022-09-30 11:42:21 +0900

    d

--- app/src/main/java/com/example/app0907/Fragment2.java
+++ app/src/main/java/com/example/app0907/Fragment2.java
@@ -54,7 +54,6 @@ public class Fragment2 extends Fragment {
         submitBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                imv
             }
         });
 
---
+++ app/src/main/java/com/example/app0907/LodingActivity.java
@@ -0,0 +1,14 @@
+package com.example.app0907;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class LodingActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_loding);
+    }
+}
