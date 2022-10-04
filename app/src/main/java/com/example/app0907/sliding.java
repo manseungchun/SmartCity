9dfcd0fee95f08abdf597b38c6090140bdaa4318
riverpurple <rkdqhfk1245@gmail.com>
2022-10-04 11:29:34 +0900

    보라작업 리셋+++

--- app/src/main/java/com/example/app0907/sliding.java
+++ app/src/main/java/com/example/app0907/sliding.java
@@ -27,7 +27,7 @@ public class sliding extends AppCompatActivity implements AdapterView.OnItemClic
 
     ListView answer_list;
 
-    //주석
+    //주석 주석주석
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
