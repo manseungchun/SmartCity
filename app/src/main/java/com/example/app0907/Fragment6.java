8f4b170dab01dd2b06ee71a87bb584151a73f9d8
smhrd <chun9695@gmail.com>
2022-10-06 16:36:37 +0900

    전체 마일리지 출력 + 마일리지 사용내역 출력

--- app/src/main/java/com/example/app0907/Fragment6.java
+++ app/src/main/java/com/example/app0907/Fragment6.java
@@ -1,14 +1,33 @@
 package com.example.app0907;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.ListView;
+import android.widget.Toast;
 
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Map;
 
 
 public class Fragment6 extends Fragment {
@@ -16,6 +35,10 @@ public class Fragment6 extends Fragment {
     // 데이터 관리할 공간
     ArrayList<MileageVO> data;
     ListView lv;
+
+    RequestQueue requestQueue;
+
+    String name;
 
 
     @Override
@@ -26,11 +49,62 @@ public class Fragment6 extends Fragment {
         data = new ArrayList<>();
         lv = view.findViewById(R.id.lv6);
 
-        data.add(new MileageVO("2022-10-04","-100"));
+        if (requestQueue == null) {
+            requestQueue = Volley.newRequestQueue(getActivity().getApplicationContext());
+        }
 
-        MileageAdapter adapter = new MileageAdapter(view.getContext(), R.layout.mileagelv, data);
+        SharedPreferences sharedPreferences = getActivity().getSharedPreferences("test", Context.MODE_PRIVATE);
+        name = sharedPreferences.getString("name","");
 
-        lv.setAdapter(adapter);
+        String url = "http://222.102.104.237:5000/point";
+
+        StringRequest request = new StringRequest(
+                Request.Method.POST,
+                url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        try {
+                            // 서버 응답 성공
+                            JSONObject jsonObject = new JSONObject(response);
+                            JSONArray jsonArray = jsonObject.getJSONArray("data");
+                            for(int i=0; i<jsonArray.length(); i++){
+                                JSONObject jsonObject1 = jsonArray.getJSONObject(i);
+                                String date = jsonObject1.getString("date");
+                                String point = jsonObject1.getString("point");
+
+                                if(!date.equals("") && !point.equals("")){
+                                    data.add(new MileageVO(date, point));
+
+                                    MileageAdapter adapter = new MileageAdapter(view.getContext(), R.layout.mileagelv, data);
+
+                                    lv.setAdapter(adapter);
+                                }
+                            }
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // 서버 응답 실패
+                        Toast.makeText(getActivity(), "마일리지 연결 실패", Toast.LENGTH_SHORT).show();
+                    }
+                }
+        ){
+            @Nullable
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                Map<String, String> params = new HashMap<>();
+                params.put("id",name);
+                return params;
+            }
+        };
+
+        request.setShouldCache(false);
+        requestQueue.add(request);
 
         return view;
 
