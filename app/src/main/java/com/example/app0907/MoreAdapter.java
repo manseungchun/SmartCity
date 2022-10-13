f58da20d28de5a8123e2df0bf39977c7c799726e
smhrd <chun9695@gmail.com>
2022-10-13 18:32:17 +0900

    신고현황에 사진 업로드

--- app/src/main/java/com/example/app0907/MoreAdapter.java
+++ app/src/main/java/com/example/app0907/MoreAdapter.java
@@ -3,14 +3,22 @@ package com.example.app0907;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
+import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MoreAdapter extends BaseAdapter {
@@ -20,6 +28,7 @@ public class MoreAdapter extends BaseAdapter {
     private int layout;
     private ArrayList<MoreVO> data;
     private LayoutInflater inflater;
+    private Bitmap bitmap;
 
     public MoreAdapter(Context context, int layout, ArrayList<MoreVO> data) {
         this.context = context;
@@ -64,6 +73,44 @@ public class MoreAdapter extends BaseAdapter {
 
         TextView order = view.findViewById(R.id.order);
         order.setText(data.get(i).getReceipt());
+
+        Thread uThread = new Thread() {
+            @Override
+            public void run() {
+                try {
+                    // 이미지 URL 경로
+                    URL img_url = new URL("http://222.102.104.237:5000/static/" + data.get(i).getImg());
+
+                    // web에서 이미지를 가져와 ImageView에 저장할 Bitmap을 만든다.
+                    HttpURLConnection conn = (HttpURLConnection) img_url.openConnection();
+                    conn.setDoInput(true); // 서버로부터 응답 수신
+                    conn.connect(); //연결된 곳에 접속할 때 (connect() 호출해야 실제 통신 가능함)
+                    InputStream is = conn.getInputStream(); //inputStream 값 가져오기
+                    bitmap = BitmapFactory.decodeStream(is); // Bitmap으로 변환
+                } catch (MalformedURLException e) {
+                    e.printStackTrace();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+
+        uThread.start(); // 작업 Thread 실행
+
+        try {
+            //메인 Thread는 별도의 작업 Thread가 작업을 완료할 때까지 대기해야 한다.
+            //join() 호출하여 별도의 작업 Thread가 종료될 때까지 메인 Thread가 기다리도록 한다.
+            //join() 메서드는 InterruptedException을 발생시킨다.
+            uThread.join();
+
+            //작업 Thread에서 이미지를 불러오는 작업을 완료한 뒤
+            //UI 작업을 할 수 있는 메인 Thread에서 ImageView에 이미지 지정
+            ImageView ivImg = view.findViewById(R.id.ivImg);
+            ivImg.setImageBitmap(bitmap);
+
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
 
         return view;
     }
